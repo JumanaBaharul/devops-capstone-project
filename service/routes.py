19e43fef9536799a3cfdb51e2738e170df80c58f
JumanaBaharul <150269305+JumanaBaharul@users.noreply.github.com>
2024-09-03 01:00:47 +0530

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -109,7 +109,6 @@ def update_accounts(account_id):
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
-    
     account.deserialize(request.get_json())
     account.update()
     return account.serialize(), status.HTTP_200_OK
@@ -148,4 +147,3 @@ def check_content_type(media_type):
         status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
         f"Content-Type must be {media_type}",
     )
-
